2ad66376c1f48d705af96b76587208de3105fa98
Feben9 <117801933+Feben9@users.noreply.github.com>
2024-04-15 22:32:30 +0300

    Created the login page

---
+++ src/Login.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+
+export default function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (username === 'John Doe' && password === 'password') {
+            alert('Login successful!');
+            setError('');
+        } else {
+            setError('Invalid username or password. Try again.');
+        }
+    };
+
+    return (
+        <div> 
+            <form className="form-container"  onSubmit={handleSubmit}>
+                <div>
+                    <label for="username" className="label">Username</label> <br/>
+                    <input 
+                        type="text" 
+                        id="username" 
+                        name="username" 
+                        value={username}
+                        className="user" 
+                        placeholder="John Doe" 
+                        onChange={(e) => setUsername(e.target.value)}
+                    /> <br/>
+                </div>
+                <br />
+                <div>
+                    <label for="password" className="label">Password</label>  <br/>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password"
+                        value={password} 
+                        className="user" 
+                        placeholder="password" 
+                        onChange={(e) => setPassword(e.target.value)}
+                    /> <br/>
+                </div>
+                <br />
+                <div>
+                    <input type="submit" value="Sign In" className="signin" />
+                </div>
+                {error && <div className="error"><br /> {error}<br /></div>}                
+            </form>
+        </div>
+    );
+}
