cd693bca4ab6e1e7017a994e94d41deb62ba56f1
Feben9 <117801933+Feben9@users.noreply.github.com>
2024-10-01 22:41:36 +0300

    Updated bulk upload

--- src/pages/BulkUpload.js
+++ src/pages/BulkUpload.js
@@ -8,7 +8,18 @@ const BulkUpload = () => {
                 <Navbar isAuthenticated={true}/>
             </div>
             <div className="page-container">
-                <h1>You can write your blogs!</h1>
+                <div style={{padding:"50px"}}></div>
+                <form className="upload-form-container"> 
+                    <button>
+                    <a style={{color:"black", textDecoration:"none"}} 
+                    href="https://file-examples.com/wp-content/storage/2017/02/file_example_XLS_10.xls" 
+                    download="sample">Download sample form</a>
+                    </button>
+                    <label for="file" className="label"></label> <br />
+                    <input type="file" id="file"/> <br />
+
+                    <input type="submit" value="Upload" /> <br />
+                </form>
             </div>
         </div>
     )
