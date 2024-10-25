5a6e711ce4a205e7862aa0d3e51acb881f4c0278
Feben <feben7351@gmail.com>
2024-10-25 16:01:53 +0300

    style

--- src/pages/BulkUpload.js
+++ src/pages/BulkUpload.js
@@ -15,8 +15,11 @@ const BulkUpload = () => {
                     href="https://file-examples.com/wp-content/storage/2017/02/file_example_XLS_10.xls" 
                     download="sample">Download sample form</a>
                     </button>
-                    <label for="file" className="label"></label> <br />
-                    <input type="file" id="file" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-1 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"/> <br />
+                    
+                    <br />
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload file</label>
+                    <input class="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" type="file" />
+                    <br />
 
                     <input type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" value="Upload" /> <br />
                 </form>
